34922daa900d1f8b8a0937129624b3ba2790652b
AKoudounis <andreaskoudounis@gmail.com>
2024-10-05 14:37:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -125,10 +125,10 @@ def serve():
     count_laureates_by_keyword_pb2_grpc.add_PrizeKeywordServiceServicer_to_server(prize_service, server)
     find_laureate_by_name_pb2_grpc.add_FindLaureateByNameServiceServicer_to_server(prize_service, server)
     
-    server.add_insecure_port('0.0.0.0:50051')
+    server.add_insecure_port('0.0.0.0:443')
 
     server.start()
-    logging.info("Server is running on port 50051...")
+    logging.info("Server is running on port 443...")
     
     try:
         while True:
